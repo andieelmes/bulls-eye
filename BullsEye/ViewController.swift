46da8b93887ee53ab3176ca81dde7e6492d44cac
z.salikhkulova <z.salikhkulova@fun-box.ru>
2019-08-29 20:17:32 +0500

    add first 14 lessons

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentSliderValue: Int = 0;
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.alert
     }
 
-
+    @IBAction func showHitMeAlert() {
+        let alert = UIAlertController(title: "Hello, World!", message: "Your slider value is \(currentSliderValue)", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        let roundedValue = slider.value.rounded()
+        currentSliderValue = Int(roundedValue);
+    }
 }
 
